d708c7368f16d30e21e7c8bb28120da5abad9b2f
Nick Clifford <ncliffor@gmail.com>
2014-11-21 16:18:23 -0500

    Reverse geocoding autofills location to form

--- app/assets/javascripts/geocoder-simple.js
+++ app/assets/javascripts/geocoder-simple.js
@@ -1,11 +1,14 @@
 $(initialize);
 
+var infowindow = new google.maps.InfoWindow();
+
 var MAP_OPTIONS = {
   zoom: 12,
 };
 
 function initialize(){
   createMap();
+  geocoder = new google.maps.Geocoder();
   addLocations();
 }
 
@@ -18,6 +21,7 @@ function createMap(){
   if (window.location.href.indexOf("dive_sites/new") > -1) {
     google.maps.event.addListenerOnce(map, "click", function(event) {
       placeMarker(event.latLng);
+      codeLatLng(event.latLng);
     });
   }
 }
@@ -33,14 +37,20 @@ function placeMarker(location){
     updateOutput(event.latLng);
   });
 
+  google.maps.event.addListener(marker, "dragend", function(event){
+    codeLatLng(event.latLng);
+  });
+
   function updateOutput(location){
       $(".lat_coords").val(marker.position.k);
-      $(".long_coords").val(marker.position.B)
+      $(".long_coords").val(marker.position.B);
+      $("#latlng").val(marker.position.k+","+marker.position.B);
 
     requestAnimationFrame(updateOutput);
   }
       $(".lat_coords").val(marker.position.k);
-      $(".long_coords").val(marker.position.B)
+      $(".long_coords").val(marker.position.B);
+      $("#latlng").val(marker.position.k+","+marker.position.B);
 }
 
 
@@ -63,6 +73,40 @@ function geocodeAndAddMarker(address){
         });
     } else {
       alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function codeLatLng() {
+  var input = document.getElementById('latlng').value;
+  var latlngStr = input.split(',', 2);
+  var lat = parseFloat(latlngStr[0]);
+  var lng = parseFloat(latlngStr[1]);
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+      $(".coords").val(results[1].formatted_address);
+      } else {
+        alert('No results found');
+      }
+    } else {
+      alert('Geocoder failed due to: ' + status);
     }
   });
 }
